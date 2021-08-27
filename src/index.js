477625f960ff9bedebb89d4db81375653328f58b
WitchDrop <witchdrop.dev@gmail.com>
2021-08-27 23:30:03 +0200

    Added replies.json Added "query all ghosts" Added "query Evidence for specific Ghost"

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 const dbConnection = require("./dbConnection")
 const express = require('express');
-const mysql = require("mysql");
-const configData = require("../local/config.json");
+const msg = require('./replies.json')
+const {response} = require("express");
 const app = express();
 const port = 8080;
-
-
 const db = dbConnection.getConnection();
-
-console.log(db);
 db.connect((err) => {
     if(err){
         throw err;
@@ -16,10 +12,37 @@ db.connect((err) => {
     console.log('Database Connected...')
 });
 
-/* app.get('/', (req, res) => {
-    res.send('Hello World!')
+//query all Ghosts currently in the game
+app.get('/ghosts', (req, res) => {
+    let sql = 'select ghost_type from ghosts';
+    let query = db.query(sql,(err,results) => {
+        if(err) throw err;
+        let ghosts = []
+        for (const i in results) {
+            ghosts.push(results[i]['ghost_type'])
+        }
+        res.send(`Ghosts[${results.length}]: ${ghosts.join(", ")}`)
+    })
+})
+//query Evidence for specific Ghost
+app.get('/ghost/:name', (req, res) => {
+    name = req.params.name.toLowerCase()
+    let sql = `SELECT evidence_type FROM ghostevidencetype where ghost_type = "${req.params.name}"`;
+    let query = db.query(sql,(err, results) => {
+        if (err) throw err;
+        if (results.length === 0) {
+            res.send(msg.ghostNotFound)
+        } else {
+        let evidence = []
+        for (const i in results) {
+            evidence.push(results[i]['evidence_type'])
+        }
+        let message = name[0].toUpperCase() + name.substring(1) + ': ' + evidence.join(', ')
+        res.send(message)
+    }
+    })
 })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-}) */
+})
