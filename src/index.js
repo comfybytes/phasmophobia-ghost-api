f244023cab03588067176e93e08836d535981273
WitchDrop <witchdrop.dev@gmail.com>
2021-08-28 03:12:42 +0200

    Added very basic 2 evidence logic and query. (needs a lot of fine tuning)

--- src/index.js
+++ src/index.js
@@ -42,7 +42,27 @@ app.get('/ghost/:name', (req, res) => {
     }
     })
 })
-
+app.get('/evidence', (req,res) => {
+    let evidence = req.query['msg'].split(' ')
+    console.log(evidence)
+    let sql;
+    if (evidence.length === 2) {
+        sql = `select g.ghost_type,e.evidence_type from ghostapi.ghostevidencetype g join ghostapi.ghostevidencetype e on g.ghost_type = e.ghost_type where (g.evidence_type like '%${evidence[0]}%' or g.evidence_type like '%${evidence[1]}%') and (e.evidence_type not like '%${evidence[0]}%' and e.evidence_type not like '%${evidence[1]}%') group by g.ghost_type having count(distinct g.evidence_type) = 2`
+    }
+    let query = db.query(sql,(err,results) => {
+        if (err) throw err;
+        let ghostEvidence = []
+        let message = 'Possible Ghost(s): ';
+        for (const i in results) {
+            message += `${results[i]['ghost_type']} (${results[i]['evidence_type']}), `
+                ghostEvidence.push({
+                ghost: results[i]['ghost_type'],
+                evidence: results[i]['evidence_type']
+            })
+        }
+        res.send(message.slice(0,-2))
+    })
+})
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
