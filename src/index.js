2aa1d7f734be52cbed6d7b60bf1736024cb23b68
WitchDrop <witchdrop.dev@gmail.com>
2021-08-28 15:50:28 +0200

    Added prepared Statements

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ const dbase = require("./dbase")
 const db = dbase.dbase
 const express = require('express');
 const msg = require('./replies.json')
-const {response} = require("express");
+//const {response} = require("express");
 const app = express();
 const port = 8080;
 
-//query all Ghosts currently in the game
-app.get('/ghosts', (req, res) => {
+app.get('/ghosts', (req, res) => { //query all Ghosts currently in the game
     let sql = 'select ghost_type from ghosts';
     db.query(sql,(err,results) => {
         if(err) throw err;
@@ -17,12 +16,13 @@ app.get('/ghosts', (req, res) => {
         }
         res.send(`Ghosts[${results.length}]: ${ghosts.join(", ")}`)
     })
-})
-//query Evidence for specific Ghost
-app.get('/ghost/:name', (req, res) => {
+});
+
+
+app.get('/ghost/:name', (req, res) => { //query Evidence for specific Ghost
     name = req.params.name.toLowerCase()
-    let sql = `SELECT evidence_type FROM ghostevidencetype where ghost_type = "${req.params.name}"`;
-    db.query(sql,(err, results) => {
+    let sql = "SELECT evidence_type FROM ghostevidencetype where ghost_type = ?";
+    db.query(sql, name, (err, results) => {
         if (err) throw err;
         if (results.length === 0) {
             res.send(msg.ghostNotFound)
@@ -38,21 +38,16 @@ app.get('/ghost/:name', (req, res) => {
 })
 app.get('/evidence', (req,res) => {
     let evidence = req.query['msg'].split(' ')
-    console.log(evidence)
     let sql;
     if (evidence.length === 2) {
-        sql = `select g.ghost_type,e.evidence_type from ghostapi.ghostevidencetype g join ghostapi.ghostevidencetype e on g.ghost_type = e.ghost_type where (g.evidence_type like '%${evidence[0]}%' or g.evidence_type like '%${evidence[1]}%') and (e.evidence_type not like '%${evidence[0]}%' and e.evidence_type not like '%${evidence[1]}%') group by g.ghost_type having count(distinct g.evidence_type) = 2`
+        sql = `select g.ghost_type,e.evidence_type from ghostevidencetype g join ghostevidencetype e on g.ghost_type = e.ghost_type where (g.evidence_type like ? or g.evidence_type like ?) and (e.evidence_type not like ? and e.evidence_type not like ?) group by g.ghost_type having count(distinct g.evidence_type) = 2`
+        evidence = [`%${evidence[0]}%`,`%${evidence[1]}`,`%${evidence[0]}%`,`%${evidence[1]}`]
     }
-    db.query(sql,(err,results) => {
+    db.query(sql,evidence,(err,results) => {
         if (err) throw err;
-        let ghostEvidence = []
         let message = 'Possible Ghost(s): ';
         for (const i in results) {
             message += `${results[i]['ghost_type']} (${results[i]['evidence_type']}), `
-                ghostEvidence.push({
-                ghost: results[i]['ghost_type'],
-                evidence: results[i]['evidence_type']
-            })
         }
         res.send(message.slice(0,-2))
     })
